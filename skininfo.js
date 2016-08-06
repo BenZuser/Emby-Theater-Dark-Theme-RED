c7f85535ee196b8848c8e801d6cc14cec441be01
Ben Z <BenZuser@users.noreply.github.com>
2016-08-06 00:47:01 -0400

    Add files via upload

--- skininfo.js
+++ skininfo.js
@@ -1,7 +1,7 @@
 define([''], function () {
 
     return {
-        id: 'defaultskin',
-        name: 'Default Skin'
+        id: 'emby.theater.dark.theme.RED',
+        name: 'Dark Theme - RED'
     };
 });
